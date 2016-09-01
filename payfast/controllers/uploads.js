8ac0713ebfb8ab2fc5cdbd7fa0cf09fa3755deac
juliocvidal <juliocvidal@gmail.com>
2016-09-01 03:26:15 -0300

    [aula4][video4] rota pra upload do arquivo

---
+++ payfast/controllers/uploads.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+module.exports = function(app){
+
+  app.post("/upload/imagem", function(req, res){
+    console.log('recebendo imagem');
+
+    var filename = req.headers.filename;
+
+    req.pipe(fs.createWriteStream('files/' + filename))
+    .on('finish', function(){
+      console.log('arquivo escrito');
+      res.status(201).send('ok');
+    });
+
+  });
+
+}
