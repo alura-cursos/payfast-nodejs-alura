e95cd5ab9d1f115053a12d6f43818bb322362114
juliocvidal <juliocvidal@gmail.com>
2016-09-01 22:28:34 -0300

    [aula7][video2] subindo a app em cluster

--- payfast/cluster.js
+++ payfast/cluster.js
@@ -1,11 +1,18 @@
 var cluster = require('cluster');
+var os = require('os');
+
+var cpus = os.cpus();
 
 console.log('executando thread');
 
 if(cluster.isMaster){
   console.log('thread master');
-  cluster.fork();
+
+  cpus.forEach(function(){
+      cluster.fork();
+  })
+
 } else {
   console.log('thread slave');
-  cluster.fork();
+  require('./index.js')
 }
